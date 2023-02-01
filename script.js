37f425d8b2e5ad83d3827e5399426ed2973707b7
Emanuel <emanuelbuzatu1@gmail.com>
2023-02-01 14:18:04 +0200

    Gata proiectul

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+
+
+
+const stringList = ['Do it!', 'Don"t!', 'Maybe next time', 'What if you take a break?', 'You are lucky today!', 'Do some more documentation.'];
+
+function generateRandomStringFromList() {
+  const randomIndex = Math.floor(Math.random() * stringList.length);
+  const result = stringList[randomIndex];
+  document.getElementById("demo").innerHTML = result;
+}
